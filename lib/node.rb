dfad77a2f817505c984e7a6b43f04c24cb0e8b6a
Anna Wiley <awiley33@gmail.com>
2023-05-12 16:50:57 -0500

    Refactor the next_node feature from a method to an instance variable

--- lib/node.rb
+++ lib/node.rb
@@ -1,11 +1,9 @@
 class Node
-    attr_reader :data
+    attr_reader :data, :next_node
 
-    def initialize (data)
+    def initialize (data, next_node = nil)
         @data = data
+        @next_node = next_node
     end
 
-    def next_node
-        
-    end
 end
