fd4993baaecd1e0a1c9a865572d79f1ca8df110c
Anna Wiley <awiley33@gmail.com>
2023-05-14 19:14:39 -0500

    Change attr_reader to attr_accessor

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,6 +1,35 @@
+
+
 class LinkedList
     attr_reader :head
     def initialize(head = nil)
         @head = head
+    end
+
+    def append(data)
+        node = @head
+
+        if head.nil?
+            head = Node.new(data)
+        else
+            until node.next_node.nil?
+                node.next_node = Node.new(data)
+            end
+        end
+        return data
+    end
+
+    def count
+        if head = nil
+            count = 0
+        else
+            node = @head
+            count = 1
+            until node.next_node.nil?
+                node = node.next_node
+                count += 1
+            end
+        end
+        count
     end
 end
