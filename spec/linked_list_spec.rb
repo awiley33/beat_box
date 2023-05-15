4fd3bf09b6da173d77d28e83514ada5c5e67f9c6
Anna Wiley <awiley33@gmail.com>
2023-05-14 22:05:50 -0500

    Remove unneeded info from tests and add else case test for count

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -2,7 +2,7 @@ require "./lib/linked_list"
 require "./lib/node"
 require "rspec"
 
-describe Node do
+describe LinkedList do
 
     it "exists" do
         list = LinkedList.new
@@ -15,6 +15,40 @@ describe Node do
 
         expect(list.head).to eq(nil)
     end
+
+    it "can append the list with a node" do
+        list = LinkedList.new
+        node = Node.new("doop")
+
+        expect(list.append("doop")).to eq("doop")
+    end
+
+    it "can append the list with multiple nodes" do
+        list = LinkedList.new
+        
+
+
+        expect(list.append("doop")).to eq("doop")
+    end
+
+    it "can count a list containing 1 node" do
+        list = LinkedList.new
+        list.append("doop")
+
+        expect(list.count).to eq(1)
+    end
+
+    it "can count a list containing more than one node" do
+        list = LinkedList.new
+        list.append("doop")
+        list.append("deep")
+
+        expect(list.count).to eq(2)
+    end
+
+    
+
+
 
 
 end
