362c2ee084d34400264c0a12980789296c6440dd
Anna Wiley <awiley33@gmail.com>
2023-05-14 21:21:14 -0500

    Remove other instance variables so that head is the only one to match interaction pattern

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,23 +2,28 @@
 
 class LinkedList
     attr_reader :head, :count
+
     def initialize
         @head = nil
-        @count = 0
-
     end
 
     def append(data)
-        @count += 1
-        node = @head
 
-        if head.nil?
-            head = Node.new(data)
+        if @head.nil?
+            @head = Node.new(data)
+            count = 1
         else
-            until node.next_node.nil?
-                node.next_node = Node.new(data)
+            current_node = @head
+            new_node = Node.new(data)
+            until current_node.next_node.nil?
+                current_node = current_node.next
             end
+            current_node.next_node = new_node
+            count += 1
         end
-        return data
+    end
+
+    def count
+        count
     end
 end
