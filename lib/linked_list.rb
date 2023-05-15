f09125182b9a18f7b1b1c21d641e37742fc77a5d
Anna Wiley <awiley33@gmail.com>
2023-05-14 22:04:36 -0500

    Debug append and count methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,7 +1,7 @@
 
 
 class LinkedList
-    attr_reader :head, :count
+    attr_reader :head
 
     def initialize
         @head = nil
@@ -11,19 +11,36 @@ class LinkedList
 
         if @head.nil?
             @head = Node.new(data)
-            count = 1
         else
             current_node = @head
             new_node = Node.new(data)
             until current_node.next_node.nil?
-                current_node = current_node.next
+                current_node = current_node.next_node
             end
             current_node.next_node = new_node
-            count += 1
+        end
+        current_node = @head
+        while current_node.next_node == nil
+            return current_node.data
         end
     end
 
     def count
+        count = 0
+        if @head.next_node.nil?
+            count = 1
+        else
+            current_node = @head
+            count = 1
+            until current_node.next_node.nil?
+                current_node = current_node.next_node
+                count += 1
+            end
+        end
         count
+    end
+
+    def to_string
+        string = ""
     end
 end
