50c4d57c42bb2c9d0747b2bde5260c67cd4f9739
Anna Wiley <awiley33@gmail.com>
2023-05-15 21:23:09 -0500

    Add the append test

--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -22,4 +22,14 @@ describe BeatBox do
         expect(bb.list.head).to eq(nil)
     end
 
+    it "can append multiple nodes at the same time" do
+        bb = BeatBox.new
+
+        expect(bb.append("deep doo ditt")).to eq("deep doo ditt")
+        expect(bb.list.head.data).to eq("deep")
+        expect(bb.list.head.next_node.data).to eq("doo")
+        expect(bb.append("woo hoo shu")).to eq("woo hoo shu")
+        expect(bb.count).to eq(6)
+    end
+
 end
