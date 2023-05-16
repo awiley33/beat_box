3fbfbc8d05230c3c4d160c01bf9f82a37ea72c88
Anna Wiley <awiley33@gmail.com>
2023-05-15 23:47:17 -0500

    Add the append and count methods to beatbox class

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -3,5 +3,27 @@ class BeatBox
     def initialize
         @list = LinkedList.new
     end
-    
+
+    def append(data)
+        if data.nil?
+            return "There is nothing here."
+        elsif data.include? " "
+            data = data.split(" ")
+            index = data.count
+            data = data.each do |n|
+                list.append(n)
+            end
+            data = list.find((list.count-index), index)
+            return data
+        else
+            list.append(data)
+            data = list.to_string
+            return data
+        end
+    end
+
+    def count
+        return list.count
+    end
+
 end
